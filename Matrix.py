b2a24d631dbc27e9f322b97590c66c11e495e66f
IronBeardX <alejandroyeroval@gmail.com>
2022-11-11 02:13:16 -0500

    Indexing attributes with patches

--- Matrix.py
+++ Matrix.py
@@ -1,10 +1,20 @@
 
+
 class Matrix:
     def __init__(self, dims, fill):
-		
         self.rows = dims[0]
         self.cols = dims[1]
         self.A = [[fill] * self.cols for i in range(self.rows)]
+        self.create_indexing_attributes()
+
+    def create_indexing_attributes(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                '''
+                BUG: if i change the matrix this will return ceros, change it without
+                having to change the value every time i make a change in the matrix
+                '''
+                self.__dict__['_' + str(i) + '_' + str(j)] = self.A[i][j] 
 
     def __str__(self):
         m = len(self.A) # Get the first dimension
@@ -35,7 +45,8 @@ class Matrix:
                 for j in range(self.cols):
                     C.A[i][j] = self.A[i][j] + other
 
-
+        #TODO: Remove this when fixed
+        self.create_indexing_attributes()
         return C
 
 	#Right addition can be done by calling left addition
@@ -58,15 +69,16 @@ class Matrix:
                 for j in range(self.cols):
                     C.A[i][j] = self.A[i][j] * other
 
+        #TODO: Remove this when fixed
+        self.create_indexing_attributes()
         return C 
 
 	#Point-wise multiplication is also commutative
     def __rmul__(self, other):
-
         return self.__mul__(other)
 
-
-    def __matmul__(self, other): #matrix-matrix multiplication
+    #matrix-matrix multiplication
+    def __matmul__(self, other): 
 
         if isinstance(other, Matrix):
             C = Matrix( dims = (self.rows, self.cols), fill = 0)
@@ -82,6 +94,8 @@ class Matrix:
 
                     C.A[i][j] = acc
 
+        #TODO: Remove this when fixed
+        self.create_indexing_attributes()
         return C
 
     def __getitem__(self, key):
@@ -96,8 +110,28 @@ class Matrix:
             i = key[0]
             j = key[1]
             self.A[i][j] = value
+            self.__dict__['_' + str(i) + '_' + str(j)] = self.A[i][j] 
+
 
     def __iter__(self):
         for row in self.A:
             for item in row:
                 yield item
+
+mat = Matrix((3,3), 0)
+mat[0,0] = 1
+mat[0,1] = 2
+mat[0,2] = 3
+mat[1,0] = 4
+mat[1,1] = 5
+mat[1,2] = 6
+mat[2,0] = 7
+mat[2,1] = 8
+mat[2,2] = 9
+
+print(mat._0_0)
+print(mat._0_1)
+print(mat._0_2)
+for i in range(3):
+    for j in range(3):
+        exec('print(mat._' + str(i) + '_' + str(j) + ')')
